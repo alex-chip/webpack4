b43ddcf0f32d75c364d40b55ad8982fce273234a
Alex Chip <alexchip32@gmail.com>
2020-07-15 15:51:20 -0600

    Introduccion al uso de Plugins

---
+++ plugins/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/js/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCSSExtractPlugin.loader
+          },
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './index.html')
+    }),
+    new MiniCSSExtractPlugin({
+      filename: 'css/[name].css'
+    })
+  ]
+}
