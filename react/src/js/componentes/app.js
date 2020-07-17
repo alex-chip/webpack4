c0c4086ded7383ce73748601fb6bda958aaedec6
Alex Chip <alexchip32@gmail.com>
2020-07-17 14:44:19 -0600

    Agregando soporte a ReactJS

---
+++ react/src/js/componentes/app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      Hola desde React.js
+    </div>
+  )
+}
+
+export default App
---
+++ react/src/js/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import '../css/index.css';
+import App from './componentes/app';
+
+render(<App/>, document.getElementById('container'))
